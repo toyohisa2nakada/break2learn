cd3367b666e7d6988bf889e57c99079d6fa72c68
nakada toyohisa <nakada.toyohisa@gmail.com>
2026-06-12 23:22:06 +0900

    html lang ja

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col pb-8">{children}</body>
